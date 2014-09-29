46eccbd1bae86cc9ae121b73cebbc08af629e813
jackzhang1204 <jackzhang1204@gmail.com>
2014-09-29 19:01:10 +0800

    update

--- sewise/sewise_player/player/html/skins/vodMobileTransparent/skin.html.js
+++ sewise/sewise_player/player/html/skins/vodMobileTransparent/skin.html.js
@@ -11,23 +11,22 @@ window.SewisePlayerSkin.localSkin =
 	'<div class="sewise-player-ui">' + 
 			'<div class="controlbar">' + 
 				'<div class="controlbar-bg">控制栏背景</div>' + 
+				'<div class="controlbar-progress">' + 
+					'<div class="controlbar-progress-totalline"></div>' + 
+					'<div class="controlbar-progress-loadedline"></div>' + 
+					'<div class="controlbar-progress-playedline"></div>' + 
+					'<div class="controlbar-progress-seekline"></div>' + 
+					'<div class="controlbar-progress-playpoint"></div>' + 
+				'</div>' + 
 				'<ul class="controlbar-btns">' + 
 					'<li><a href="javascript:;" class="controlbar-btns-play" tabindex="1">播放</a></li>' + 
-					'<li><a href="javascript:;" class="controlbar-btns-pause" tabindex="1">暂停</a></li>' + 
-					'<li><a href="javascript:;" class="controlbar-btns-live" tabindex="2">直播</a></li>' + 
-					'<div class="controlbar-progress">' + 
-						'<div class="controlbar-progress-totalline"></div>' + 
-						'<div class="controlbar-progress-loadedline"></div>' + 
-						'<div class="controlbar-progress-playedline"></div>' + 
-						'<div class="controlbar-progress-seekline"></div>' + 
-						'<div class="controlbar-progress-playpoint" tabindex="3"></div>' + 
-					'</div>' + 
-					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-fullscreen" tabindex="6">全屏</a></li>' + 
-					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-normalscreen" tabindex="6">普屏</a></li>' + 
+					'<li><a href="javascript:;" class="controlbar-btns-pause" tabindex="2">暂停</a></li>' + 
+					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-fullscreen" tabindex="1">全屏</a></li>' + 
+					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-normalscreen" tabindex="1">普屏</a></li>' + 
 					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-soundclose" tabindex="1">音量关闭</a></li>' + 
 					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-soundopen" tabindex="1">音量打开</a></li>' + 
-					'<!-- <li class="right-float"><a href="javascript:;" class="controlbar-btns-share" tabindex="1">分享</a></li> -->' + 
-					'<div class="controlbar-playtime">00:00:00/00:00:00</div>' + 
+					'<li class="right-float"><a href="javascript:;" class="controlbar-btns-share" tabindex="1">分享</a></li>' + 
+					'<div class="controlbar-playtime">000:00/000:00</div>' + 
 				'</ul>' + 
 			'</div>' + 
 			'<div class="logo">' + 
